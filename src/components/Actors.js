7221067ce8c180d4633819c27db40a1b2d32698f
Sugut <sugutvictorine@gmail.com>
2022-09-29 18:56:06 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>{/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index)=>{
+        return(
+          <div key={index}>
+          name={actor.name}
+          <ul>
+             <li>{actor.movies}</li>
+          </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return(
+   <div>{/*{code here}*/}
+    <h1>Movies Page</h1>
+    {movies.map((movie, index)=>{
+      return (
+        <div key ={index}>
+        title={movie.title}
+        time= {movie.time}
+          <ul>
+            <li>{movie.genres}</li>
+          </ul>
+      </div>
+      )
+    })}
+  </div>
+)}
 
 export default Movies;
